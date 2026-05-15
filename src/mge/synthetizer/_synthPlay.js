f04cff2c157e25e8a7091bb36780dcdbd9e1d995
Homer Dilpleu <homer.dilpleu@yahoo.com>
2026-05-15 10:03:23 +0200

    Minr update

--- src/mge/synthetizer/_synthPlay.js
+++ src/mge/synthetizer/_synthPlay.js
@@ -5,11 +5,11 @@ mge._synth._synthetizer._synthPlay = function (_frequency, _startTime, _duration
     if (!isNaN(_frequency)) {
         // Check if the sound is already pre-rendered
         if (this._preRenderedSounds.get (this._synthGetCacheId(_frequency, _duration)) == undefined) {
-            console.log('Real time')
+            //console.log('Real time')
             this._synthPlayLive(_frequency, _startTime, _duration, _volume)
             this._synthPreRender(_frequency, _duration)
         } else {
-            console.log('Pre rendered')
+            //console.log('Pre rendered')
             this._synthPlayPreRendered(_frequency, _startTime, _duration, _volume)
         }
     }
