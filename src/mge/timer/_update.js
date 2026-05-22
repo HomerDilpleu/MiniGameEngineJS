4f61dbe1b066d2f9ed6ef227cf345a14753f9d6c
Homer Dilpleu <homer.dilpleu@yahoo.com>
2026-05-22 17:47:54 +0200

    Timer: add tests

--- src/mge/timer/_update.js
+++ src/mge/timer/_update.js
@@ -4,7 +4,7 @@
 mge._timer._update = function () {
 
     if (this._mode == 'L') {
-        this._progress = (Date.now() - this._startTimestamp) % this._duration
+        this._progress = (Date.now() - this._startTimestamp) % this._duration / this._duration
     } else {
         this._progress = Math.min(1,(Date.now() - this._startTimestamp)/this._duration)
     }
