af18df9b5570a93bec76c32b55fea6bc26b88ee2
Homer Dilpleu <homer.dilpleu@yahoo.com>
2026-07-05 22:06:26 +0200

    Camera: add example

--- build/mge_Vx.x.x.js
+++ build/mge_Vx.x.x.js
@@ -736,8 +736,8 @@ mge._camera._worldToScreen = function(_sprite) {
         // Else it position and size is imapcted
         _result._xScreen = (_sprite._x - (mge._camera._x - mge.game.width/2) * _sprite._scrollFactor) * mge._camera._zoom + mge.game.width/2
         _result._yScreen = (_sprite._y - (mge._camera._y - mge.game.height/2) * _sprite._scrollFactor) * mge._camera._zoom + mge.game.height/2
-        _result._scaleXScreen = _sprite._scaleX * mge._camera._zoom
-        _result._scaleYScreen = _sprite._scaleY * mge._camera._zoom
+        _result._scaleXScreen = _sprite._scaleX * mge._camera._zoom * _sprite._scrollFactor
+        _result._scaleYScreen = _sprite._scaleY * mge._camera._zoom * _sprite._scrollFactor
     }
    return _result
 }
